ddc037c03d48ac607dc60c02f63efcd272a8c2c1
ReFlecT21 <89706651+ReFlecT21@users.noreply.github.com>
2025-05-10 03:05:32 +0800

    feat: enhance ViewAsset component with navigation and loading states

--- portal-production/containers/ViewAsset/index.tsx
+++ portal-production/containers/ViewAsset/index.tsx
@@ -10,10 +10,14 @@ import useStatusCountsHandler from "./hooks/useStatusCountsHandler";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import useEditAssetHandler from "../Assets/hooks/useEditAssetHandler";
 import useGetAsset from "./hooks/useGetAsset";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/routes";
 
 export default function ViewAsset() {
   const params = useParams();
   const theme = useTheme();
+  const router = useRouter();
   const skuKey = params.skuKey;
   const { asset, category, isGetAssetLoading } = useGetAsset();
   const { inventoriesStatusCounts, isGetInventoriesLoading } = useStatusCountsHandler();
@@ -24,87 +28,98 @@ export default function ViewAsset() {
   return (
     <Box sx={{ gap: "var(--default-gap)", display: "flex", flexDirection: "column" }}>
       <Typography variant="body1" color="text.secondary">
-        Asset / <strong>{skuKey}</strong>
+        <Box component="span" sx={{ cursor: "pointer" }} onClick={() => router.push(ROUTES.ASSETS)}>
+          <strong>Asset</strong>
+        </Box>{" "}
+        / {skuKey}
       </Typography>
 
-      <Grid2 container spacing={3} sx={{ mt: 2 }}>
-        <Grid2 size={{ xs: 12, md: 6 }}>
-          {isGetAssetLoading ? (
-            <Skeleton variant="rectangular" width={400} height={300} sx={{ maxWidth: "100%", borderRadius: 2 }} />
-          ) : (
-            <Avatar src={`${process.env.NEXT_PUBLIC_RESOURCE_URL}${asset?.image}`} alt={asset?.name.toString().slice(0, 2).toUpperCase() || "NA"} sx={{ width: 400, height: 300, fontSize: 32, maxWidth: "100%", maxHeight: "300px" }} variant="rounded" />
-          )}
-        </Grid2>
-
-        <Grid2 size={{ xs: 12, md: 6 }}>
-          <Grid2 container>
-            <Grid2 size={{ xs: 12, md: 6 }}>
-              <Stack direction="column" gap="var(--default-gap)">
+      <Grid2 container spacing={3}>
+        <Grid2 container spacing={3} justifyContent="space-between" alignItems="flex-start">
+          <Grid2 item xs={12} md={4}>
+            {isGetAssetLoading ? (
+              <Skeleton variant="rectangular" width={400} height={300} sx={{ maxWidth: "100%", borderRadius: 2 }} />
+            ) : (
+              <Avatar src={`${process.env.NEXT_PUBLIC_RESOURCE_URL}${asset?.image}`} alt={asset?.name.toString().slice(0, 2).toUpperCase() || "NA"} sx={{ width: 400, height: 300, fontSize: 32, maxWidth: "100%", maxHeight: "300px" }} variant="rounded" />
+            )}
+          </Grid2>
+          <Grid2 item xs={12} md={4} sx={{ display: "flex", justifyContent: "flex-end" }}>
+            <Stack direction="column" gap="var(--default-gap)">
+              {isGetAssetLoading ? (
+                <Skeleton variant="text" width={200} height={32} />
+              ) : (
+                <Typography variant="h5" fontWeight="bold">
+                  {asset?.name}
+                </Typography>
+              )}
+              <Stack direction="column">
                 {isGetAssetLoading ? (
-                  <Skeleton variant="text" width={200} height={32} />
+                  <Skeleton variant="text" width={180} />
                 ) : (
-                  <Typography variant="h5" fontWeight="bold">
-                    {asset?.name}
+                  <Typography variant="body1" color="text.secondary">
+                    Category: {category?.name}
                   </Typography>
                 )}
-                <Stack direction="column">
-                  {isGetAssetLoading ? (
-                    <Skeleton variant="text" width={180} />
-                  ) : (
-                    <Typography variant="body1" color="text.secondary">
-                      Category: {category?.name}
-                    </Typography>
-                  )}
-                  {isGetAssetLoading ? (
-                    <Skeleton variant="text" width={100} height={32} />
-                  ) : (
-                    <Typography variant="body1" color="text.secondary">
-                      Asset Details: {asset?.description}
-                    </Typography>
-                  )}
-                </Stack>
+                {isGetAssetLoading ? (
+                  <Skeleton variant="text" width={100} height={32} />
+                ) : (
+                  <Typography variant="body1" color="text.secondary">
+                    Asset Details: {asset?.description}
+                  </Typography>
+                )}
               </Stack>
-            </Grid2>
-            <Grid2 size={{ xs: 12, md: 6 }}>
-              <Stack spacing={2}>
-                {" "}
-                {/* You can tweak spacing (e.g., 1.5, 2.5) */}
-                <Box display="flex" gap="var(--half-gap)" alignItems="center">
-                  {isGetAssetLoading ? (
-                    <Skeleton variant="text" width={100} height={32} />
-                  ) : (
-                    <Typography variant="body1" color="text.secondary">
-                      Edit Asset
-                    </Typography>
-                  )}
-                  {isGetAssetLoading ? (
-                    <Skeleton variant="rectangular" width={40} height={40} />
-                  ) : (
-                    <IconButton
-                      onClick={() => handleEdit(asset?.id)}
-                      sx={{
-                        borderRadius: "8px",
-                        color: "secondary.contrastText",
-                        bgcolor: "secondary.main",
-                        "&:hover": { bgcolor: "secondary.dark" },
-                      }}
-                    >
-                      <ModeEditIcon />
-                    </IconButton>
-                  )}
-                </Box>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: "var(--half-gap)", alignItems: "flex-start" }}>
-                  {isGetAssetLoading ? (
-                    <Skeleton variant="text" width={100} height={32} />
-                  ) : (
-                    <Typography variant="body1" color="text.secondary">
-                      Inventory
-                    </Typography>
-                  )}
-                  {isGetAssetLoading ? <Skeleton variant="rectangular" width={100} height={40} /> : <Button variant="contained">View</Button>}
-                </Box>
-              </Stack>
-            </Grid2>
+            </Stack>
+          </Grid2>
+        </Grid2>
+
+        <Grid2 item xs={12} md={4} sx={{ display: "flex", justifyContent: "flex-end", pr: 4, ml: "auto" }}>
+          <Grid2
+            item
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              width: "100%",
+              maxWidth: "100%",
+            }}
+          >
+            <Stack spacing={2} sx={{ alignItems: "flex-end", width: "100%", maxWidth: 200 }}>
+              {" "}
+              {/* You can tweak spacing (e.g., 1.5, 2.5) */}
+              <Box display="flex" gap="var(--half-gap)" alignItems="center">
+                {isGetAssetLoading ? (
+                  <Skeleton variant="text" width={100} height={32} />
+                ) : (
+                  <Typography variant="body1" color="text.secondary">
+                    Edit Asset
+                  </Typography>
+                )}
+                {isGetAssetLoading ? (
+                  <Skeleton variant="rectangular" width={40} height={40} />
+                ) : (
+                  <IconButton
+                    onClick={() => handleEdit(asset?.id)}
+                    sx={{
+                      borderRadius: "8px",
+                      color: "secondary.contrastText",
+                      bgcolor: "secondary.main",
+                      "&:hover": { bgcolor: "secondary.dark" },
+                    }}
+                  >
+                    <ModeEditIcon />
+                  </IconButton>
+                )}
+              </Box>
+              <Box sx={{ display: "flex", flexDirection: "column", gap: "var(--half-gap)", alignItems: "flex-start" }}>
+                {isGetAssetLoading ? (
+                  <Skeleton variant="text" width={100} height={32} />
+                ) : (
+                  <Typography variant="body1" color="text.secondary">
+                    Inventory
+                  </Typography>
+                )}
+                {isGetAssetLoading ? <Skeleton variant="rectangular" width={100} height={40} /> : <Button variant="contained">View</Button>}
+              </Box>
+            </Stack>
           </Grid2>
         </Grid2>
       </Grid2>
